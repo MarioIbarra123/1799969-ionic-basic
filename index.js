829ef194c12205a01a95bcc96ba8786bc48f0b91
Couch <tovareduardo75@gmail.com>
2023-08-18 18:45:43 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var x ="Hola mundo"
+var mitexto1txt = document.getElementsById(´mitexto1´);
+function mostrarAlerta(){
+    alert(mitexto1txt.value);
+}
+function cambiarTexto(){
+    mitexto1txt.value = "hola a todos";
+}
+.mayusculas {
+text-transform: uppercase;    
+}
+
